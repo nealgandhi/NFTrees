73442092baff9763e96b699908de2018911b39d1
CharlesAverill <charlesaverill20@gmail.com>
2021-03-26 22:45:08 -0500

    basic django stuff

--- nftree/main/urls.py
+++ nftree/main/urls.py
@@ -1,4 +1,4 @@
-"""nftree URL Configuration
+"""main URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name="index"),
 ]
