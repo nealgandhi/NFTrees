b2b8f37139e333f5d8dddb7cd0da1523ffb47bab
CharlesAverill <charlesaverill20@gmail.com>
2021-03-28 00:12:36 -0500

    update installed_apps

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nftree.main.wsgi
--- nftree/main/settings.py
+++ nftree/main/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
-    'tokens',
+    'nftree.accounts',
+    'nftree.tokens',
 ]
 
 MIDDLEWARE = [
