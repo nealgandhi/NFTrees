986f8d9b05bb757fc98452c8d87e8d7feaf2c95d
CharlesAverill <charlesaverill20@gmail.com>
2021-03-28 02:27:11 -0500

    temp auction stuff

--- nftree/tokens/urls.py
+++ nftree/tokens/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('index', views.index, name="index"),
     path('auctions', views.auctions, name="auctions"),
     path('auctions/new', views.new_auction, name="new_auction"),
+    path('auctions/<slug:token>', views.auction_details, name="auction_details"),
     path('tokens/<slug:token>', views.token_details, name="token_details")
 ]
